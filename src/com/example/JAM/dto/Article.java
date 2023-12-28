339a4d6e70d26100898dad12ef52937530530c55
LEE, Dongsoo <waslds@daum.net>
2023-12-28 13:27:32 +0900

    JDBCSelectTest

--- src/com/example/JAM/dto/Article.java
+++ src/com/example/JAM/dto/Article.java
@@ -3,12 +3,24 @@ package com.example.JAM.dto;
 public class Article {
 	public int id;
 	
+	public String regDate;
+	
+	public String updateDate;
+	
 	public String title;
 	
 	public String body;
 	
 	public Article(int id, String title, String body) {
 		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+
+	public Article(int id, String regDate, String updateDate, String title, String body) {
+		this.id = id;
+		this.regDate = regDate;
+		this.updateDate = updateDate;
 		this.title = title;
 		this.body = body;
 	}
